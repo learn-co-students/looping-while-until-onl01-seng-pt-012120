6e5c24fe3f716f5aa36c46bc49f5c095ca306549
Aaron Wilson <codingsensei19@gmail.com>
2020-01-29 02:26:19 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
-end
+until counter > 3
+puts counter
+counter = counter +1
 
+end
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
   levitation_force = 6
-	
+while counter < 3
+puts counter
+counter = counter + 1
   #your code here
 end
-
-
