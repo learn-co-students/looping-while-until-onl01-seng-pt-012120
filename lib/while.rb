f9951f42acadda52e91fecd06bcec45c7591efa2
Patricia C <patricia@patriciaco.com>
2020-02-09 02:57:55 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,14 +1,14 @@
-# def using_while
-#   levitation_force = 6
-# 	puts " Wingardium Leviosa"
-	
-# end
-
 def using_while
-  levitation_force = 6
-	while levitation_force < 10 
-	puts 'Wingardium Leviosa'
-  levitation_force += 1 
-end 
-end
+levitation_force = 6
+while levitation_force < 10
+  puts "Wingardium Leviosa"
+  levitation_force += 1
+  end
+ end
+ 
+
+
+
+ 
+
 
